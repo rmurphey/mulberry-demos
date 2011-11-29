970120a5663536de8b79b01f6def0331013a3477
Rebecca Murphey <rmurphey@gmail.com>
2011-11-29 13:29:32 -0500

    fix whitespace

--- recipes/javascript/components/HomeHeader.js
+++ recipes/javascript/components/HomeHeader.js
@@ -5,30 +5,30 @@ dojo.provide('client.components.HomeHeader');
 dojo.declare('client.components.HomeHeader', [ toura.components.HeaderImage ], {
   templateString: dojo.cache('client.components', 'HomeHeader/HomeHeader.haml'),
   "class": 'home-header',
-  
+
   prepareData: function() {
     this.inherited(arguments);
-    
+
     this.title = this.node ? this.node.name : this.title;
-    
+
     if (this.viewImage) {
       dojo.mixin(this.viewImage, this._calculateDimensions());
     }
   },
-  
+
   postCreate: function() {
     this.inherited(arguments);
-    
+
     this._offsetImage();
   },
-  
+
   _offsetImage: function() {
     var img = this.image,
         h = this._getHeight(),
         $node = $(this.imageNode),
         imgheight = $node.height(),
         voffset = img ? imgheight - h/2 : 0;
-    
+
     // voffset should always be positive
     if(voffset < 0) { voffset = 0; }
     $node.css('margin-top', -voffset);
@@ -36,12 +36,12 @@ dojo.declare('client.components.HomeHeader', [ toura.components.HeaderImage ], {
     // also fire /content/update to be sure scrollers all still work
     dojo.publish('/content/update', [{}]);
   },
-  
+
   _resizeImage: function() {
     this.inherited(arguments);
     this._offsetImage();
   },
-    
+
   _getWidth: function() {
     return toura.app.UI.viewport.width;
   },
