0c8a0bd1f581d1151b4381e067abffcf787af6d7
Rebecca Murphey <rmurphey@gmail.com>
2011-11-29 13:32:06 -0500

    simpler way of using node data w/template

--- recipes/javascript/components/IngredientsList.js
+++ recipes/javascript/components/IngredientsList.js
@@ -2,17 +2,8 @@ dojo.provide('client.components.IngredientsList');
 
 mulberry.component('IngredientsList', {
   componentTemplate : dojo.cache('client.components', 'IngredientsList/IngredientsList.haml'),
-  ingredientTemplate: dojo.cache('client.components', 'IngredientsList/Ingredient.haml'),
 
   prep : function() {
-    this.data = this.node.data[0];
-    this.ingredients = this.data.json.ingredients;
-  },
-
-  setupChildComponents : function() {
-    // replace with populate once it's available
-    for( var i = 0, l = this.ingredients.length, el; i < l; ++i ){
-      this.domNode.innerHTML += "<li>"+this.ingredients[i]+"</li>";
-    }
+    this.ingredients = this.node.getData('recipe').ingredients;
   }
 });
