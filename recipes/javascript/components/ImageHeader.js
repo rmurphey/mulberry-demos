51b695a76287a73d2ea6c6ffc076862a87c92192
Rebecca Murphey <rmurphey@gmail.com>
2011-11-29 13:31:43 -0500

    get background image widget working

--- recipes/javascript/components/ImageHeader.js
+++ recipes/javascript/components/ImageHeader.js
@@ -3,20 +3,15 @@ dojo.provide('client.components.ImageHeader');
 mulberry.component('ImageHeader', {
   componentTemplate : dojo.cache('client.components', 'ImageHeader/ImageHeader.haml'),
 
+  widgetsInTemplate : true,
+
   prep : function() {
     this.title = this.node ? this.node.name : this.title;
-    this.image = this.node ? this.node.featuredImage : false;// this.node ? this.node.images[0].original.url : false;
+    this.image = this.node ? this.node.featuredImage : false;
   },
 
   init : function() {
-    //console.log(this);
-    // gotta fix this eventually to use the BackgroundImage class right
-    this.$domNode.css('background-image', 'url('+this.image.large.url+')')
-  },
-  
-  // WHY DOES THIS FUNCTION NOT RUN?
-  postCreate: function() {
-    //toura.ui.BackgroundImage.loadImage();
+    this.headerNode.set('backgroundImage', this.image.large);
+    this.headerNode.loadImage();
   }
-  
 });
