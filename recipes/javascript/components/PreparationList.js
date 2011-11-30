651c518094b402ca493423fed555dede3f9849d4
Evan Hensleigh <ehensleigh@toura.com>
2011-11-30 17:21:39 -0500

    Beginning to style the recipe pages.

--- recipes/javascript/components/PreparationList.js
+++ recipes/javascript/components/PreparationList.js
@@ -5,5 +5,16 @@ mulberry.component('PreparationList', {
 
   prep : function() {
     this.preparations = this.node.getData('recipe').preparation;
-  }
+  }/* FOR LATER,
+
+  init : function() {
+    var $lis = $(this.domNode).children('li');
+    // provide a random number for the styling...
+
+    $lis.each(function() {
+      console.log(this);
+      $(this).css('-webkit-transform', 'rotate(' + Math.rand * 4 - 2 + 'deg)');
+    });
+    //dojo.style(this.domNode, '-webkit-transform', 'rotate('+Math.rand * 4 - 2+')deg');
+  }*/
 });
