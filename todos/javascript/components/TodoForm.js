e27dffb454dbe321a8948256ae7d1023fdc3b367
Rebecca Murphey <rmurphey@gmail.com>
2011-12-05 22:07:14 -0500

    cleanup

--- todos/javascript/components/TodoForm.js
+++ todos/javascript/components/TodoForm.js
@@ -4,15 +4,11 @@ mulberry.component('TodoForm', {
   componentTemplate : dojo.cache('client.components', 'TodoForm/TodoForm.haml'),
 
   init : function() {
-    this.connect(this.saveButton, 'click',  function(e) {
-      e.preventDefault();
-      var item = { description : this.descriptionInput.value };
-      this.onAdd(item);
-      this.domNode.reset();
-    });
-
     this.connect(this.domNode, 'submit', function(e) {
       e.preventDefault();
+      var item = { description : this.descriptionInput.value };
+      this.domNode.reset();
+      this.onAdd(item);
     });
   },
 
