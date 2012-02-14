a96295fcef8282d40ba5a54bc91773b852193358
Rebecca Murphey <rmurphey@gmail.com>
2012-02-14 16:18:22 -0500

    require toura.base in demos

--- phonegap_apis/javascript/base.js
+++ phonegap_apis/javascript/base.js
@@ -2,6 +2,7 @@
  * You can write custom JavaScript here; this is also where Mulberry will add
  * any dependencies you introduce via `mulberry create component`.
  */
+dojo.require('toura.base');
 dojo.require('client.routes');
 
 dojo.require('client.components.Camera');
